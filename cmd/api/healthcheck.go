ecbc539a7e4c8c6899a9fadf2112b7d47723755f
Yersin Berdikulov <221225@astanait.edu.kz>
2024-05-11 18:13:51 +0500

    Add json marshalling instead of static response -> healthcheck.go

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResponse := `{"Status": "OK", "Environment": %q, "Version": %q}`
-	jsonResponse = fmt.Sprintf(jsonResponse, app.config.env, version)
+	data := map[string]string{
+		"Status":      "OK",
+		"Environment": app.config.env,
+		"Version":     version,
+	}
+
+	jsonResponse, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered an internal error, could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse = append(jsonResponse, '\n')
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(jsonResponse))
 }
